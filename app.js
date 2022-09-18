294b74abfc7345b580e018ee31185df35775fa14
Arman Patnaik <armanpatnaik2002son@gmail.com>
2022-09-18 15:06:51 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('button');
+button.addEventListener('click',function(){
+    const randomColor = makeRandomColor();
+    document.body.style.backgroundColor = randomColor;
+})
+const makeRandomColor = ()=>{
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+};
